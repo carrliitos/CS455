a2e6b932749240b5edf9b6e99d58034653f683cb
carrliitos <salazar.ten80@gmail.com>
2020-09-10 16:21:38 -0500

    need to add encrypt and decrypt methods

--- week2/Assignment1/PlayFairCipher/PlayfairCipher.java
+++ week2/Assignment1/PlayFairCipher/PlayfairCipher.java
@@ -1,33 +1,69 @@
-/* @author: Benzon Carlitos Salazar
-*	CS455 Homework 1, part 2 -- Using Playfair Cipher to encrypt a message
-*/
-
-import java.awt.Point;
-import java.util.HashMap;
 import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Scanner;
+import java.awt.Point;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
 
 public class PlayfairCipher {
-	// I is excluded and combined with J
-	public static final String ALPHABET[] = {"A","B","C","D","E","F","G","H","I","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z"};
-
+	public static final String ALPHABET[] = {"A","B","C","D","E","F","G","H",
+											"I","K","L","M","N","O","P","Q","R",
+											"S","T","U","V","W","X","Y","Z"};
+	
 	public static void main(String[] args) {
-		HashMap<String, Letter> alphabet = generateLetters();
+		Scanner input = new Scanner(System.in);
+		System.out.print("Choose (E)ncryption or (D)ecryption: ");
+		String choice = input.next();
+		boolean valid = false;
+		while(!valid){
+			if(choice.toUpperCase().startsWith("E")) {
+				System.out.print("Enter your text: ");
+				String plainText = input.next();
 
-		String plainText = "HELLOWORLD";
-		String key = "PASSWORD";
+				System.out.print("Enter your key: ");
+				String key = input.next();
 
-		// generate tableau
+				valid = true;
+
+				plainText = plainText.toUpperCase();
+				key = key.toUpperCase();
+			}else if(choice.toUpperCase().startsWith("D")) {
+				System.out.print("Enter your text: ");
+				String cipherText = input.next();
+
+				System.out.print("Enter your key: ");
+				String key = input.next();
+
+				valid = true;
+
+				cipherText = cipherText.toUpperCase();
+				key = key.toUpperCase();
+			}else {
+				System.out.println("Choose (E) or (D): ");
+				choice = input.next();
+			}
+		}
+
+		// populate tableau
+		HashMap<String, Letter> alphabet = populateTableau();
+		// Create tableau
 		String tableau[][] = generateTableau(key, alphabet);
-		// map each letter to their location
-		mappingInd(tableau, alphabet);
-		// print our tableau
+		// map each letter in the tableau
+		map(tableau, alphabet);
 		printTableau(tableau);
-		// Start encryption
-		encrypt(tableau, plainText, alphabet);
+
+		String result = "";
+		if(choice.equals("E")) {
+			result = encrypt(tableau, plainText, alphabet);
+			System.out.println("Encryption: " + result);
+		}else {
+			result = decrypt(tableau, cipherText, alphabet);
+			System.out.println("Decryption: " + result);
+		}
 	}
 
-	// Generate our letters
-	public static HashMap<String, Letter>generateLetters() {
+	public static HashMap<String, Letter> populateTableau() {
 		HashMap<String, Letter> letters = new HashMap<>();
 		for(int i = 0; i < ALPHABET.length; i++) {
 			letters.put(ALPHABET[i], new Letter(ALPHABET[i]));
@@ -35,99 +71,92 @@ public class PlayfairCipher {
 		return letters;
 	}
 
-	private static void mappingInd(String[][] matrix, HashMap<String, Letter> alphabet) {
-		for(int i = 0; i < matrix[0].length; i++) {
-			for(int j = 0; j < matrix.length; j++) {
-				Point point = new Point(i, j);
-				alphabet.get(matrix[i][j]).setPoint(point);
-			}
-		}
-	}
-
-	// Generate 5x5 tableau
-	public static String[][] generateTableau(String key, HashMap<String, Letter> letters){
-		// Make sure our key is in uppercase, removing anything that isn't a letter, and replace I with J
-		key = key.toUpperCase();
-		key = key.replaceAll("[^A-Z]", "");
+	public static String[][] generateTableau(String key, HashMap<String, Letter> letters) {
+		// replace I with J
 		key = key.replace("I", "J");
-
-		// Create ArrayList for removing duplicates for the key
+		// create temp for removing any duplicates
 		ArrayList<String> temp = new ArrayList<>();
 
-		// Walk through the length of the key word
 		for(int i = 0; i < key.length(); i++) {
-			// check each letter if it has been used already in the key, if not, add to the key and set it as used
+			// checking if the letter in the key has been used already
 			for(int j = 0; j < letters.size(); j++) {
-				String currentLetter = key.substring(i, i + 1);
-				if(!letters.get(currentLetter).getUsed()) {
-					temp.add(currentLetter);
-					letters.get(currentLetter.setUsed(true));
+				String current = key.substring(i, i + 1);
+				if(!letters.get(current).getUsed()) {
+					temp.add(current);
+					letters.get(current).setUsed(true);
 				}
 			}
 		}
 
-		// Add remaining letters of the alphabet
+		// add the rest of the alphabet to the key
 		for(int i = 0; i < letters.size(); i++) {
-			if(!letters.get(ALPHABET[i].getUsed())) {
+			if(!letters.get(ALPHABET[i]).getUsed()) {
 				temp.add(letters.get(ALPHABET[i]).getLetter());
 			}
 		}
 
-		// Add to tableau
-		String tableau[][] = new String[5][5];
+		// Add to the tableau
+		String matrix[][] = new String[5][5];
 		int count = 0;
 		for(int k = 0; k < 5; k++) {
 			for(int l = 0; l < 5; l++) {
-				tableau[k][l] = temp.get(count);
+				matrix[k][l] = temp.get(count);
 				count++;
 			}
 		}
-
-		return tableau;
+		return matrix;
 	}
 
-	// print the tableau
-	public static void printTableau(String tableau[][]) {
-		System.out.println("**TABLEAU**");
-		for(int i = 0; i < tableau.length; i++) {
-			for(int j = 0; 0 < tableau[0].length; j++) {
-				System.out.print(tableau[i][j] + " ");
+	private static void map(String[][] tableau, HashMap<String, Letter> alphabet) {
+		for(int i = 0; i < tableau[0].length; i++) {
+			for(int j = 0; j < tableau.length; j++) {
+				Point point = new Point(i, j);
+				alphabet.get(tableau[i][j]).setPoint(point);
+			}
+		}
+	}
+
+	public static void printTableau(String matrix[][]) {
+		System.out.println("**PLAYFAIR TABLEAU**");
+		for(int i = 0; i < matrix.length; i++) {
+			for(int j = 0; j < matrix[0].length; j++) {
+				System.out.print(matrix[i][j] + " ");
 			}
 			System.out.println();
 		}
 	}
 
-	// encryption starts here
+	public static String pair(String text) {
+		text = text.replace("I", "J");
+		int textLength = text.length();
+		StringBuilder pairedText = new StringBuilder(text);
+		for(int i = 0; i < textLength - 1; i += 2) {
+			if(pairedText.charAt(i) == pairedText.charAt(i + 1)) {
+				pairedText.insert(i + 1, "X");
+				textLength++;
+			}
+		}
+
+		// add X at the end if textLength is odd
+		if(pairedText.length() % 2 != 0) pairedText.append("X");
+		text = pairedText.toString();
+		return text;
+	}
+
 	private static String encrypt(String[][] tableau, String plainText, HashMap<String, Letter> letters) {
 		StringBuilder cipherText = new StringBuilder();
 
-		// walk through the plaintext
-		for(int i = 0; i < plainText.length(); i += 2) {
-			String one = plainText.substring(i, i + 1);
-			String two = plainText.substring(i + 1, i + 2);
+		for(int i = 0; i < plainText.length(); i += 2) { // Remember: pairs
+			String letter1 = plainText.substring(i, i + 1);
+			String letter2 = plainText.substring(i + 1, i + 2);
 
-			// find the x, y coordinates of letter one and two
-			int x1 = (int)((letters.get(one).getPoint().getX()));
-			int x2 = (int)((letters.get(two).getPoint().getX()));
-			int y1 = (int)((letters.get(one).getPoint().getY()));
-			int y2 = (int)((letters.get(two).getPoint().getY()));
+			// Identify x and y coords for the pair
+			int x1 = (int) ((letters.get(letter1).getPoint().getX()));
+			int x2 = (int) ((letters.get(letter2).getPoint().getX()));
+			int y1 = (int) ((letters.get(letter1).getPoint().getY()));
+			int y2 = (int) ((letters.get(letter2).getPoint().getY()));
 
-			// If letters are on the same row, shift -- accounting for the wrapping
-			if(x1 == x2) {
-				y1 = (y1 + 1) % 5;
-				y2 = (y2 + 1) % 5;
-				cipherText.append(tableau[x1][y1]);
-				cipherText.append(tableau[x2][y2]);
-			}else if(y1 == y2){ // If letters are on the same col, shift -- accounting for the wrapping
-				x1 = (x1 + 1) % 5;
-				x2 = (x2 + 1) % 5;
-				cipherText.append(tableau[x1][y1]);
-				cipherText.append(tableau[x2][y2]);
-			}else { // otherwise, swap the values of both letters
-				cipherText.append(tableau[x1][y2]);
-				cipherText.append(tableau[x2][y1]);
-			}
+			// Check 
 		}
-		return cipherText.toString();
 	}
 }
