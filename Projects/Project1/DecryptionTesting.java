f009afa918721c2a0948e1f60d44a5ceea0550d1
carrliitos <salazar.ten80@gmail.com>
2020-09-25 13:55:21 -0500

    finished all frequency finds and recovered the plaintext

--- Projects/Project1/DecryptionTesting.java
+++ Projects/Project1/DecryptionTesting.java
@@ -1,10 +1,16 @@
 import java.util.*;
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
 
 public class DecryptionTesting {
     public static void main(String [] args) {
         String ciphertext = "EHK NDGBKOE XCEKJIKDX KIGGS GL EHK POUDFKE FKEEKD, IT JUEHUJVKF HUMEHGDJK";
-        String key = "xzpsacitwnehgfmlqjvyrodbuk";
-        String KEY = "XZPSACITWNEHGFMLQJVYRODBUK";
+        String key = "uioyklxhvbsfwjgnrdpeczmatq";
+        String KEY = "UIOYKLXHVBSFWJGNRDPECZMATQ";
         
         String alphabet = "abcdefghijklmnopqrstuvwxyz";
         String ALPHABET = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
--- Projects/Project1/Monoalphabetic.java
+++ Projects/Project1/Monoalphabetic.java
@@ -8,29 +8,49 @@
 */
 
 import java.io.File;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.BufferedReader;
 import java.io.FileNotFoundException;
+import java.io.IOException;
 import java.util.Scanner;
+import java.util.ArrayList;
 import java.util.Arrays;
 
 public class Monoalphabetic {
-	public static final String filepath = "./Benzon S.txt";
-
 	public static void main(String[] args) {
-		int chars[] = readFile();
+		String filepath = "Benzon S.txt";
+
+		int chars[] = letterFrequency(filepath);
 		if(chars == null) return;
 
-		for(int i = 0; i < 26; i++) {
-			char letter = (char)('A' + i);
-			System.out.println(letter + "," + chars[i]);
+		try {
+			File f = new File("letterFrequency.txt");
+			FileWriter fWriter = new FileWriter(f);
+
+			fWriter.write("LETTER\t| FREQUENCY\n");
+			fWriter.write("--------|----------\n");
+			for(int i = 0; i < 26; i++) {
+				char letter = (char)('A' + i);
+				fWriter.write(letter + "\t\t| " + chars[i] + "\n");
+			}
+			fWriter.flush();
+			fWriter.close();
+		}catch(IOException e) {
+			System.out.println("IO Error in main() method.");
+			e.printStackTrace();
 		}
+
+		decrypt(filepath);
 	}
 
-	public static int[] readFile() {
+	public static int[] letterFrequency(String filepath) {
 		try {
 			File file = new File(filepath);
 			Scanner reader = new Scanner(file, "UTF-8");
 
 			int ret[] = new int[26];
+			int totalLetter = 0;
 
 			while(reader.hasNextLine()) {
 				String line = reader.nextLine().toUpperCase();
@@ -41,11 +61,54 @@ public class Monoalphabetic {
 					}
 				}
 			}
+
 			reader.close();
 			return ret;
 		}catch(FileNotFoundException e) {
-			System.out.println("Cannot find file.");
+			System.out.println("Cannot find file in letterFrequency() method.");
+			e.printStackTrace();
 			return null;
+		}
+	}
+
+	public static void decrypt(String filePath) {
+		try {
+			File file = new File(filePath);
+			FileReader fReader = new FileReader(file);
+			BufferedReader bReader = new BufferedReader(fReader);
+
+			File f = new File("plainText.txt");
+			FileWriter fWriter = new FileWriter(f);
+
+			int c = 0;
+			ArrayList<Character> chars = new ArrayList<>();
+			while((c = bReader.read()) != -1) {
+				char character = (char) c;
+				chars.add(character);
+			}
+			String cipherText = "";
+			for(int i = 0; i < chars.size(); i++) {
+				cipherText += chars.get(i);
+			}
+			String KEY = "UIOYKLXHVBSFWJGNRDPECZMATQ";
+			String ALPHABET = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+
+			int i, j;
+			for(i = 0; i < cipherText.length(); i++) {
+				for(j = 0; j < KEY.length(); j++) {
+					if(cipherText.charAt(i) == KEY.charAt(j)) {
+						fWriter.write(ALPHABET.charAt(j));
+					}
+				}
+			}
+
+			fReader.close();
+			bReader.close();
+			fWriter.flush();
+			fWriter.close();
+		}catch(IOException e) {
+			System.out.println("IO Error in decrypt() method");
+			e.printStackTrace();
 		}
 	}
 }
